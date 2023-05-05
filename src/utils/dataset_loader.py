c7698a8c152150250ee8ab12d5b36a1fba5221a2
Jadocee <jaydoncameron80+github@gmail.com>
2023-05-05 11:56:30 +1000

    Added a dataset loader utility class (experimental)

--- src/utils/dataset_loader.py
+++ src/utils/dataset_loader.py
@@ -1,23 +1,24 @@
-from datasets import load_dataset
+from typing import List, Tuple
+
+import datasets
+from datasets import load_dataset, load_dataset_builder, get_dataset_split_names, Dataset, DatasetDict
 
 
 class DatasetLoader:
 
-    def __init__(self):
+    def __init__(self) -> None:
         pass
 
     @staticmethod
-    def get_dataset(dataset_name: str):
-        if dataset_name == "tweet_topic_single":
-            train_data = load_dataset(dataset_name, split="train_coling2022")
-            test_data = load_dataset(dataset_name, split="test_coling2022")
-            train_valid_split = train_data.train_test_split(test_size=0.2)
-            train_data = train_valid_split["train"]
-            valid_data = train_valid_split["test"]
-            # train_data = train_data.with_format(type="torch", columns=["text", "date", "label", "label_name", "id"])
-            # valid_data = valid_data.with_format(type="torch", columns=["text", "date", "label", "label_name", "id"])
-            # test_data = test_data.with_format(type="torch", columns=["text", "date", "label", "label_name", "id"])
+    def get_dataset_split_names(dataset_name: str) -> List[str]:
+        return get_dataset_split_names(dataset_name)
 
-            return train_data, valid_data, test_data
-        else:
-            raise Exception("Unknown dataset")
+    @staticmethod
+    def get_dataset(dataset_name: str, train_split: str, test_split: str, valid_size: float = 0.2) \
+            -> Tuple[Dataset, Dataset, Dataset]:
+        train_data: Dataset = load_dataset(dataset_name, split=train_split)
+        test_data: Dataset = load_dataset(dataset_name, split=test_split)
+        train_valid_split: DatasetDict = train_data.train_test_split(test_size=valid_size)
+        train_data: Dataset = train_valid_split["train"]
+        valid_data: Dataset = train_valid_split["test"]
+        return train_data, valid_data, test_data
