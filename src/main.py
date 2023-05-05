4333b9c7f9fcaafc0d3616a623133a697c1c9623
Jadocee <jaydoncameron80+github@gmail.com>
2023-05-05 11:52:33 +1000

    Started work on command line menu

--- src/main.py
+++ src/main.py
@@ -1,6 +1,50 @@
-def main():
-    print("Hello World!")
+from os import system, name
+from typing import Dict
+
+
+class Main:
+    MENUS: Dict[str, Dict[int, str]] = {
+        "Main Menu": {
+            1: "Image Classifier",
+            2: "Tweet Classifier",
+        },
+        "Tweet Classifier Menu": {
+            1: "Train",
+            2: "Test",
+            3: "Predict",
+        }
+    }
+
+    def __init__(self) -> None:
+        raise Exception("This class is not meant to be instantiated")
+
+    @staticmethod
+    def __print_menu(menu: str) -> str:
+        if menu not in Main.MENUS.keys():
+            raise Exception("Unknown menu")
+
+        for key, value in Main.MENUS[menu].items():
+            print(f"{key}. {value}")
+        print("0. Main Menu")
+        print("-1. Exit")
+        choice: int = int(input("Enter choice: "))
+        if choice == -1:
+            print("Exiting...")
+            exit(0)
+        elif choice == 0:
+            return "Main Menu"
+        elif choice not in Main.MENUS[menu].keys():
+            print("Invalid choice. Try again.")
+            system("cls" if name == "nt" else "clear")
+            Main.__print_menu(menu)
+        return Main.MENUS[menu][choice]
+
+    @staticmethod
+    def main() -> None:
+        next_menu: str = Main.__print_menu("Main Menu")
+        Main.__print_menu(next_menu)
+        # TODO: Implement menu functionality
 
 
 if __name__ == '__main__':
-    main()
+    Main.main()
