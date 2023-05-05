b8deffa7ca0e910055781be8fc714e7f553efc57
Jadocee <jaydoncameron80+github@gmail.com>
2023-05-05 17:42:35 +1000

    Test for downloading Tweet Topic Single Dataset splits

--- tests/test_dataset_loader.py
+++ tests/test_dataset_loader.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 from utils.dataset_loader import DatasetLoader
 from datasets import load_dataset, load_dataset_builder, get_dataset_split_names
 
+from utils.definitions import TWEET_TOPIC_SINGLE, TWEET_TOPIC_SINGLE_TRAIN_SPLIT, TWEET_TOPIC_SINGLE_TEST_SPLIT
 
 
 class TestDatasetLoader(TestCase):
@@ -10,6 +11,15 @@ class TestDatasetLoader(TestCase):
         ds_builder = load_dataset_builder("cardiffnlp/tweet_topic_single")
         print(ds_builder.info.description)
         print(ds_builder.info.features)
+        train_data, valid_data, test_data = DatasetLoader.get_dataset(
+            dataset_name=TWEET_TOPIC_SINGLE,
+            train_split=TWEET_TOPIC_SINGLE_TRAIN_SPLIT,
+            test_split=TWEET_TOPIC_SINGLE_TEST_SPLIT
+        )
+        print(train_data)
+        print(valid_data)
+        print(test_data)
+
 
 
 
