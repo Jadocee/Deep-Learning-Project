2a3bc0358d98a0957d2d9b6ea603c2b188c297d7
Jadocee <jaydoncameron80+github@gmail.com>
2023-05-12 12:21:06 +1000

    fixes

--- src/trainers/bow_classifier_trainer.py
+++ src/trainers/bow_classifier_trainer.py
@@ -22,20 +22,20 @@ from torchtext.vocab import Vocab
 
 class BOWClassifierTrainer(BaseTrainer):
 
-
     def save(self):
         raise NotImplementedError
 
     def load(self):
         raise NotImplementedError
-    
-    def __init__(self, train_dataloader: DataLoader, valid_dataloader: DataLoader, test_dataloader: DataLoader, vocab: Vocab, device: str = "cpu") -> None:
+
+    def __init__(self, train_dataloader: DataLoader, valid_dataloader: DataLoader, test_dataloader: DataLoader,
+                 vocab: Vocab, device: str = "cpu") -> None:
         super().__init__(device=device, train_dataloader=train_dataloader, valid_dataloader=valid_dataloader,
-                            test_dataloader=test_dataloader)
+                         test_dataloader=test_dataloader)
         self.__vocab = vocab
         self.__pad_index = vocab["<pad>"]
-        
-    def evaluate(model, dataloader, loss_fn):
+
+    def evaluate(self, model: BOWModel, dataloader, loss_fn):
         model.eval()
         losses, accuracies = [], []
         with torch.no_grad():
@@ -52,8 +52,8 @@ class BOWClassifierTrainer(BaseTrainer):
             accuracy = torch.sum(torch.argmax(preds, dim=-1) == labels) / labels.shape[0]
             accuracies.append(accuracy.detach().cpu().numpy())
         return np.mean(losses), np.mean(accuracies)
-    
-    def train(model, dataloader, loss_fn, optimizer):
+
+    def train(self, model: BOWModel, dataloader, loss_fn, optimizer):
         model.train()
         losses, accuracies = [], []
         for batch in dataloader:
@@ -75,9 +75,8 @@ class BOWClassifierTrainer(BaseTrainer):
             accuracies.append(accuracy.detach().cpu().numpy())
         return np.mean(losses), np.mean(accuracies)
 
-
     def run(self,
-            model: LSTMModel,
+            model: BOWModel,
             epochs: int = 5,
             batch_size: int = 128,
             learning_rate: float = 0.01,
@@ -88,7 +87,6 @@ class BOWClassifierTrainer(BaseTrainer):
             # lr_decay: Optional[float] = None,
             kwargs: Optional[Dict] = None
             ) -> float:
-      
 
         optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
         loss_fn = CrossEntropyLoss()
@@ -97,13 +95,15 @@ class BOWClassifierTrainer(BaseTrainer):
         valid_losses, valid_accuracies = [], []
         for epoch in range(10):
             # Train
-            train_loss, train_accuracy = BOWClassifierTrainer.train(model, self._train_dataloader, loss_fn, optimizer)
+            train_loss, train_accuracy = self.train(model, self._train_dataloader, loss_fn, optimizer)
             # Evaluate
-            valid_loss, valid_accuracy = BOWClassifierTrainer.evaluate(model, self._valid_dataloader, loss_fn)
+            valid_loss, valid_accuracy = self.evaluate(model, self._valid_dataloader, loss_fn)
             # Log
             train_losses.append(train_loss)
             train_accuracies.append(train_accuracy)
             valid_losses.append(valid_loss)
             valid_accuracies.append(valid_accuracy)
             print("Epoch {}: train_loss={:.4f}, train_accuracy={:.4f}, valid_loss={:.4f}, valid_accuracy={:.4f}".format(
-                epoch+1, train_loss, train_accuracy, valid_loss, valid_accuracy))
+                epoch + 1, train_loss, train_accuracy, valid_loss, valid_accuracy))
+
+        return valid_accuracies[-1]
